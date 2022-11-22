846799d3feb2801350b03c93530f173afabc4307
BinitaPareek <114767283+BinitaPareek@users.noreply.github.com>
2022-11-22 12:49:26 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+'''
+Created on 20-Nov-2022
+
+@author: Binita
+'''
+age=0
+birth_year=(int)(input("Enter your birth year : "))
+current_year=(int)(input("Enter the current year : "))
+name=(input("Enter our name : "))
+age=current_year-birth_year
+print(name+" is ",age," years old.")
